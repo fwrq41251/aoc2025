6e4cde39b65945000e414e597c9136657273bbf8
winry <fwrq41251@gmail.com>
2025-12-13 18:17:07 +0900

    day1

--- day01/main.go
+++ day01/main.go
@@ -56,5 +56,46 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	return 0
+	currentPos := 50
+	zeroCount := 0
+
+	for _, line := range lines {
+		prevPos := currentPos
+		direction := line[0]
+		numberStr := line[1:]
+
+		steps, err := strconv.Atoi(numberStr)
+		if err != nil {
+			fmt.Println("Error converting steps:", err)
+			continue
+		}
+
+		switch direction {
+		case 'R':
+			currentPos += steps
+		case 'L':
+			currentPos -= steps
+		}
+
+		if currentPos == 0 {
+			zeroCount++
+		} else if currentPos < 0 {
+			toAdd := ((-currentPos) / 100)
+			if prevPos != 0 {
+				toAdd++
+			}
+			zeroCount += toAdd
+		} else if currentPos >= 100 {
+			toAdd := (currentPos / 100)
+			zeroCount += toAdd
+		}
+
+		currentPos = currentPos % 100
+
+		if currentPos < 0 {
+			currentPos += 100
+		}
+	}
+
+	return zeroCount
 }
