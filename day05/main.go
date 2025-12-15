aafe7c10d896e547f3e39b37d261f695bb8aba51
winry <fwrq41251@gmail.com>
2025-12-15 16:50:28 +0900

    day5

--- day05/main.go
+++ day05/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -19,10 +21,72 @@ func main() {
 	fmt.Println("Part 2:", part2(lines))
 }
 
+type Range struct {
+	start, end int
+}
+
 func part1(lines []string) int {
-	return 0
+	ranges := []Range{}
+	freshCount := 0
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		if strings.Contains(line, "-") {
+			parts := strings.Split(line, "-")
+			start, _ := strconv.Atoi(parts[0])
+			end, _ := strconv.Atoi(parts[1])
+			ranges = append(ranges, Range{start: start, end: end})
+		} else {
+			num, _ := strconv.Atoi(line)
+			for _, r := range ranges {
+				if num >= r.start && num <= r.end {
+					freshCount++
+					break
+				}
+			}
+		}
+	}
+	return freshCount
 }
 
 func part2(lines []string) int {
-	return 0
+	ranges := []Range{}
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if strings.Contains(line, "-") {
+			parts := strings.Split(line, "-")
+			start, _ := strconv.Atoi(parts[0])
+			end, _ := strconv.Atoi(parts[1])
+			ranges = append(ranges, Range{start: start, end: end})
+		}
+	}
+
+	sort.Slice(ranges, func(i, j int) bool {
+		return ranges[i].start < ranges[j].start
+	})
+
+	mergedRanges := []Range{}
+	mergedRanges = append(mergedRanges, ranges[0])
+	for i := 1; i < len(ranges); i++ {
+		current := ranges[i]
+
+		lastIndex := len(mergedRanges) - 1
+		last := mergedRanges[lastIndex]
+
+		if current.start <= last.end {
+			mergedRanges[lastIndex].end = max(last.end, current.end)
+		} else {
+			mergedRanges = append(mergedRanges, current)
+		}
+	}
+
+	freshCount := 0
+	for _, r := range mergedRanges {
+		freshCount += (r.end - r.start + 1)
+	}
+	return freshCount
 }
