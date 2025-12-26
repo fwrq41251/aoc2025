1d3e97175ea93e7e28ab894b711222a371c3b839
winry <fwrq41251@gmail.com>
2025-12-26 17:27:25 +0900

    day11

--- day11/main.go
+++ day11/main.go
@@ -20,9 +20,68 @@ func main() {
 }
 
 func part1(lines []string) int {
-	return 0
+	adjacencyMap := make(map[string][]string)
+	for _, line := range lines {
+		node, neighbors := parseLine(line)
+		adjacencyMap[node] = neighbors
+	}
+
+	return getPathCount(adjacencyMap, "you", "out", make(map[string]int))
 }
 
-func part2(grid []string) int {
-	return 0
+func parseLine(line string) (string, []string) {
+	before, after, _ := strings.Cut(line, ":")
+	node := before
+	leftStr := strings.TrimSpace(after)
+	neighbors := strings.Split(leftStr, " ")
+
+	return node, neighbors
+}
+
+// getPathCount returns the number of distinct paths from start to end using DFS with memoization.
+func getPathCount(adjacencyMap map[string][]string, start string, end string, cache map[string]int) int {
+	if start == end {
+		return 1
+	}
+
+	if val, found := cache[start]; found {
+		return val
+	}
+
+	totalPaths := 0
+	for _, neighbor := range adjacencyMap[start] {
+		totalPaths += getPathCount(adjacencyMap, neighbor, end, cache)
+	}
+
+	cache[start] = totalPaths
+	return totalPaths
+}
+
+func part2(lines []string) int {
+	adjacencyMap := make(map[string][]string)
+	for _, line := range lines {
+		node, neighbors := parseLine(line)
+		adjacencyMap[node] = neighbors
+	}
+
+	start := "svr"
+	end := "out"
+	ponit1 := "dac"
+	point2 := "fft"
+
+	dacToFftPaths := getPathCount(adjacencyMap, ponit1, point2, make(map[string]int))
+	if dacToFftPaths > 0 {
+		startToDacPaths := getPathCount(adjacencyMap, start, ponit1, make(map[string]int))
+		fftToOutPaths := getPathCount(adjacencyMap, point2, end, make(map[string]int))
+		return startToDacPaths * dacToFftPaths * fftToOutPaths
+	} else {
+		fftToDacPaths := getPathCount(adjacencyMap, point2, ponit1, make(map[string]int))
+		if fftToDacPaths == 0 {
+			return 0
+		} else {
+			startToFftPaths := getPathCount(adjacencyMap, start, point2, make(map[string]int))
+			dacToOutPaths := getPathCount(adjacencyMap, ponit1, end, make(map[string]int))
+			return startToFftPaths * fftToDacPaths * dacToOutPaths
+		}
+	}
 }
